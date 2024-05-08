d5b3f724ed771fd5acc7a099f58e5e13a76455e7
Leonardo3003 <20183812@aloe.ulima.edu.pe>
2024-05-08 01:22:37 -0500

    se creo el proyecto en react , ademas se esta probrando el uso de componentes

---
+++ src/cuadroform.jsx
@@ -0,0 +1,14 @@
+
+import React,{ useState } from 'react'
+
+export default function cuadroform() {
+  
+  const [text] = useState()
+  return (
+    <div>
+      <input type="text" />
+      
+    </div>
+  )
+}
+
