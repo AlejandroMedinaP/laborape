806a4c3aacc69fec74db0a6f45e3410871bfbb50
LuisCCris <carbajalluis480@gmail.com>
2024-05-15 23:50:15 -0500

    p7

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,31 +1,78 @@
-import React, { Component } from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeSection: "home"
+    };
+  }
+
+  handleNavClick = (section) => {
+    this.setState({ activeSection: section });
+    //alert(`${section}`)
+  };
+
   render() {
+    const { activeSection } = this.state;
+
     return (
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Mi Aplicación</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#publicacion">Publicación</Nav.Link>
-            <NavDropdown title="Categorías" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/1">Personal</NavDropdown.Item>
-              <NavDropdown.Item href="#action/2">Trabajo</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3">Estudios</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/4">Otra Categoría</NavDropdown.Item>
-            </NavDropdown>
-            {/* Puedes agregar más elementos de navegación según sea necesario */}
-          </Nav>
-          <Nav>
-            <Nav.Link href="#login">Iniciar Sesión</Nav.Link>
-            <Nav.Link href="#signup">Registrarse</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <header>
+        <Navbar
+          expand="sm"
+          className={`bg-body-tertiary Navbar ${
+            activeSection === "home" ? "home-active" : ""
+          }`}
+          bg="dark"
+          variant="dark"
+          fixed="top"
+        >
+          <Container className="container-nav">
+            <Navbar.Brand href="/">ChongApi</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+                <Nav.Link
+                  href="/home"
+                  onClick={() => this.handleNavClick("home")}
+                >
+                  Home
+                </Nav.Link>
+                <Nav.Link
+                  href="/about"
+                  onClick={() => this.handleNavClick("about")}
+                >
+                  About
+                </Nav.Link>
+                <Nav.Link
+                  href="/local"
+                  onClick={() => this.handleNavClick("Locales")}
+                >
+                  Locales
+                </Nav.Link>
+                <Nav.Link
+                  href="/review"
+                  onClick={() => this.handleNavClick("review")}
+                >
+                  Review
+                </Nav.Link>
+                <NavDropdown
+                  title="Servicios"
+                  id="nav-dropdown"
+                  menuVariant="dark"
+                >
+                  <NavDropdown.Item href="#action3">Ofertas</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action4">Locales</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">YOLO</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
     );
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
+
 export default function IndexPage() {
   const router = useRouter();
 
