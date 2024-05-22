2c4d6fbbfd9923d0e45877e69320903bccc33c01
LuisCCris <carbajalluis480@gmail.com>
2024-05-22 00:53:10 -0500

    p10

--- src/pages/propuestas.js
+++ src/pages/propuestas.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import LogoBar from '@/component/LogoBar';
 import LaboraPe from '../Imagenes/LaboraPe.png';
-const Propuestas = () => {
-  return (
-    <div>
-      <LogoBar />
-      <h1>Mis Propuestas</h1>
-    </div>
-    
-  );
-};
+class propuestas extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      propuestas: [
+        {
+          id: 1,
+          imagen: 'ruta-imagen-1',
+          nombre: 'Nombre Propuesta 1',
+          descripcion: 'Descripción breve de la Propuesta 1'
+        },
+        {
+          id: 2,
+          imagen: 'ruta-imagen-2',
+          nombre: 'Nombre Propuesta 2',
+          descripcion: 'Descripción breve de la Propuesta 2'
+        },
+        // Puedes agregar más propuestas según sea necesario
+      ]
+    };
+  }
 
-export default Propuestas; 
+  render() {
+    const { propuestas } = this.state;
+
+    return (
+      <div className="container">
+        <LogoBar></LogoBar>
+        <h1>Freelancer Proposals for Job Applicant</h1>
+        {propuestas.map(propuesta => (
+          <div key={propuesta.id} className="propuesta-card">
+            <img src={propuesta.imagen} alt="Imagen Propuesta" />
+            <h2>{propuesta.nombre}</h2>
+            <p>{propuesta.descripcion}</p>
+            <div className="botones">
+              <button>View Profile</button>
+              <button>Aceptar</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default propuestas;
