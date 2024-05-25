155ecfe961373f7ff3952437ec7de3bc9393c591
AlejandroShyo <alejandro.medina96577@gmail.com>
2024-05-24 23:54:07 -0500

    Avance página propuestaContratista

---
+++ src/pages/propuestasContratista.js
@@ -0,0 +1,4 @@
+import { useState } from 'react';
+import { Button, Box, Grid, Typography, Card, CardActions, CardContent, CardMedia, Pagination } from '@mui/material';
+import PropuestaCard from '@/components/PropuestaCard';
+import LogoBar from '@/components/LogoBar';
